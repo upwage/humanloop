431b86223486d89bbbe66b7ad5fa10873f68bad0
Andrei Bratu <andrei@humanloop.com>
2025-02-19 10:55:52 +0000

    hl sdk fix

--- services/api/workbench/src/app.module.ts
+++ services/api/workbench/src/app.module.ts
@@ -4,11 +4,22 @@ import { SwaggerService } from './swagger.service';
 import { Module } from '@nestjs/common';
 import { Logger } from '@nestjs/common';
 import { WorkbenchModule } from './workbench/workbench.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { BearerStrategy } from './auth/bearer.strategy';
+import { AppDataSource } from 'ormconfig';
 
 @Module({
-  imports: [WorkbenchModule],
+  imports: [
+    WorkbenchModule,
+    TypeOrmModule.forRoot({
+      ...AppDataSource.options,
+      // Warned to not set this true in production
+      // Circumvents the need to run migrations
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
-  providers: [AppService, SwaggerService, Logger],
+  providers: [AppService, SwaggerService, Logger, BearerStrategy],
 })
 export class AppModule {
   constructor() {}
